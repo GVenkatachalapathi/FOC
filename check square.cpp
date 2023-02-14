29595863f64dd3ffba98645dc6fff3e75ac17b78
GVenkatachalapathi <122212678+GVenkatachalapathi@users.noreply.github.com>
2023-02-14 18:20:45 +0530

    Add files via upload

---
+++ check square.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,x=0,v=0,i=1,temp,k=0;
+	printf("enter a number");
+	scanf("%d",&n)
+	n=temp;
+	for(i=1;i<=n;i++) 
+	{
+	k==i*i;
+	}
+ 	if(k==n)
+ 	{
+ 	printf("it is a square");
+	}
+ 	return 0;
+}
